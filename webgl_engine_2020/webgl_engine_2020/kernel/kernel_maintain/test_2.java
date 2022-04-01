f9da175f8584f916a0866284f413db2e08555dc5
Fuyan Liu <lfy@shu.edu.cn>
2022-04-01 09:53:05 +0800

    commit-2022-04-01

--- webgl_engine_2020/webgl_engine_2020/kernel/kernel_maintain/test_2.java
+++ webgl_engine_2020/webgl_engine_2020/kernel/kernel_maintain/test_2.java
@@ -1,15 +1,20 @@
 package kernel_maintain;
 
 import kernel_common_class.debug_information;
-import kernel_file_manager.file_writer;
+import kernel_file_manager.file_reader;
 
 public class test_2
 {
 	public static void main(String args[])
 	{
 		debug_information.println("Begin");
-			file_writer.delete_comment(
-				"E:\\water_all\\data\\project\\boat\\part\\200米舟桥架设\\data\\solidworks\\舟桥器材\\标准梁节.SLDASM.mesh.face", null);
+		
+		file_reader fr=new file_reader("E:\\cad\\test\\part.obj.mesh",null);
+		
+		new kernel_part.part_rude(fr);
+		
+		fr.close();
+		
 		debug_information.println("End");
 	}
 }
