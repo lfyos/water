4e65c4511d3a01e3a100fcfc8cdadaa2c277e1c2
Fuyan Liu <lfy@shu.edu.cn>
2022-05-18 00:18:52 +0800

    commit-2022-05-18

--- webgl_engine_2020/webgl_engine_2020/driver_controller/driver_component_marker/component_marker.java
+++ webgl_engine_2020/webgl_engine_2020/driver_controller/driver_component_marker/component_marker.java
@@ -7,17 +7,18 @@ public class component_marker
 	private static long system_marker_id=0;
 	
 	public long marker_id;
-	public String marker_component_name,marker_text;
+	public int marker_component_id;
+	public String marker_text;
 	public double marker_x,marker_y,marker_z;
 	
 	public component_marker(component my_mark_comp,String my_marker_text,
 			double my_marker_x,double my_marker_y,double my_marker_z)
 	{
-		marker_id				=system_marker_id++;
-		marker_component_name	=my_mark_comp.component_name;
-		marker_text				=my_marker_text;
-		marker_x				=my_marker_x;
-		marker_y				=my_marker_y;
-		marker_z				=my_marker_z;
+		marker_id			=system_marker_id++;
+		marker_component_id	=my_mark_comp.component_id;
+		marker_text			=my_marker_text;
+		marker_x			=my_marker_x;
+		marker_y			=my_marker_y;
+		marker_z			=my_marker_z;
 	}
 }
