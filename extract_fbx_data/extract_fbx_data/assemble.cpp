8de2e89d560568af4b997b34d0fc64abb23e4d20
Fuyan Liu <lfy@shu.edu.cn>
2022-06-27 22:32:57 +0800

    commit-2022-06-27

--- extract_fbx_data/extract_fbx_data/assemble.cpp
+++ extract_fbx_data/extract_fbx_data/assemble.cpp
@@ -11,11 +11,16 @@ assemble::assemble(part& part_collector, std::ofstream& f, FbxNode* assemble_nod
 	int children_number = assemble_node->GetChildCount();
 
 	if (assemble_name.size() <= 0)
-		assemble_name = "no_assemble_name_"+std::to_string(assemble_id);
+		assemble_name = "no_assemble_name_"+std::to_string(component_id);
 	assemble_name = assemble_parent_name + "/" + assemble_name;
 
 	output_space(f, space_number);
-	f << "/*	assemble name	*/	" << assemble_name << "	/*	ID:"<< assemble_id <<"	*/" << std::endl;
+	
+	f << "/*	assemble name	*/	" << assemble_name;
+	f << "	/*	component_id:" << component_id;
+	f << "	leaf_component_id:" << leaf_component_id << "	*/";
+	f << std::endl;
+
 	output_space(f, space_number);
 	f << "/*	part name		*/	" << part_name	<< std::endl;
 	output_space(f, space_number);
@@ -23,16 +28,17 @@ assemble::assemble(part& part_collector, std::ofstream& f, FbxNode* assemble_nod
 	
 	for (int i=0; i < 4; i++)
 		for (int j = 0; j < 4; j++)
-			f << "	" << (loca.Get(j, i));
+			f << "	" << (loca.Get(i, j));
 	f << std::endl;
 
 	output_space(f, space_number);
 	f << "/*	child number	*/	" << children_number << std::endl;
 
-	assemble_id++;
+	component_id++;
+	leaf_component_id += (children_number > 0) ? 0 : 1;
 
 	for (int i = 0; i < children_number; i++)
 		assemble assem(part_collector, f, assemble_node->GetChild(i), space_number + 1, assemble_name);
 }
 
-int assemble_id = 0;
+int component_id=0, leaf_component_id = 0;
