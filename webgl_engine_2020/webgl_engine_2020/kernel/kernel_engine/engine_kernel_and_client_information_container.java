eef623f3c3d6dacc548c5841bb252bc43fc70151
Fuyan Liu <lfy@shu.edu.cn>
2022-07-17 02:58:48 +0800

    commit-2022-07-17-2

--- webgl_engine_2020/webgl_engine_2020/kernel/kernel_engine/engine_kernel_and_client_information_container.java
+++ webgl_engine_2020/webgl_engine_2020/kernel/kernel_engine/engine_kernel_and_client_information_container.java
@@ -17,8 +17,7 @@ public class engine_kernel_and_client_information_container
 	public client_information 		client_information;
 	public volatile int 			access_lock_number;
 	
-	public engine_kernel_and_client_information_container(
-			engine_kernel_container my_engine_kernel_cont)
+	public engine_kernel_and_client_information_container(engine_kernel_container my_engine_kernel_cont)
 	{
 		engine_kernel_cont	=my_engine_kernel_cont;
 		client_information	=null;
