aa693a1d4dda7094b4b0e9357346dc8248088fd6
Fuyan Liu <lfy@shu.edu.cn>
2022-10-16 23:58:21 +0800

    commit-2022-10-16

--- webgl_engine_2022/webgl_engine_2022/format_converter/format_convert/protected_cadex_converter.java
+++ webgl_engine_2022/webgl_engine_2022/format_converter/format_convert/protected_cadex_converter.java
@@ -1,4 +1,4 @@
-package old_convert;
+package format_convert;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -8,38 +8,39 @@ import kernel_common_class.execute_command;
 import kernel_file_manager.file_reader;
 import kernel_file_manager.file_writer;
 
-public class protected_cadex_converter
+public class protected_cadex_converter 
 {
 	public static void main(String args[])
 	{
+//		args=new String[] {"1800000","c:\\tmp\\1030-P-211-3D.stp","c:\\temp\\","e:\\cad\\lfy.jar"};
+		
 		debug_information.println("Begin");
-		do_convert(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[6]);
+		do_convert(args[0],args[1],args[2],args[3]);
 		debug_information.println("End");
 	}
-	public static boolean do_convert(
-			String source_file_name,String target_directory_name,String jar_file_name,
-			String max_convert_time_length_str,String memory_string,String max_step_number,
-			String precision,String external_flag)
+	public static boolean do_convert(String max_convert_time_length_str,
+			String source_file_name,String target_directory_name,String jar_file_name)
 	{
+		return do_convert("1024",max_convert_time_length_str,
+				source_file_name,target_directory_name,jar_file_name,"0","0","100");
+	}
+	public static boolean do_convert(String memory_string,String max_convert_time_length_str,
+			String source_file_name,String target_directory_name,String jar_file_name,
+			String chordal_deflection_str,String angular_deflection_str,String max_step_number_str)
+	{
+		debug_information.println("protected_cadex_converter memory_string:		",			memory_string);
+		debug_information.println("protected_cadex_converter max_convert_time_length:	",	max_convert_time_length_str);
 		debug_information.println("protected_cadex_converter source_file_name:		",		source_file_name);
 		debug_information.println("protected_cadex_converter target_directory_name:	",		target_directory_name);
 		debug_information.println("protected_cadex_converter jar_file_name:		",			jar_file_name);
-		debug_information.println("protected_cadex_converter max_convert_time_length:	",	max_convert_time_length_str);
-		debug_information.println("protected_cadex_converter memory_string:		",			memory_string);
-		
-		int index_id,max_convert_time_length=Integer.decode(max_convert_time_length_str);
-		
-		String source_directory_name;
-		source_file_name=file_reader.separator(source_file_name);
-		if((index_id=source_file_name.lastIndexOf(File.separator))<0)
-			source_directory_name="."+File.separator;
-		else {
-			source_directory_name=source_file_name.substring(0,index_id+1);
-			source_file_name=source_file_name.substring(index_id+1);
-		}
-		
+		debug_information.println("protected_cadex_converter chordal_deflection:	",		chordal_deflection_str);
+		debug_information.println("protected_cadex_converter angular_deflection:	",		angular_deflection_str);
+		debug_information.println("protected_cadex_converter max_step_number:	",			max_step_number_str);
+
 		String config_directory_name;
 		jar_file_name=file_reader.separator(jar_file_name);
+		
+		int index_id;
 		if((index_id=jar_file_name.lastIndexOf(File.separator))<0)
 			config_directory_name="."+File.separator;
 		else 
@@ -58,21 +59,20 @@ public class protected_cadex_converter
 			{
 				debug_information.println(result);
 			}
-			public exec_converter(String name,String cmd[],String par[])
+			public exec_converter(String name,String cmd[])
 			{
-				super(name,cmd,par,max_convert_time_length);
+				super(name,cmd,null,Long.decode(max_convert_time_length_str));
 			}
 		};
-
 		new exec_converter(source_file_name,new String[]
 				{
 					"java","-Xms256m","-Xmx"+memory_string.trim()+"m",
 					"-classpath",jar_file_name,"format_convert.cadex_converter",
-
-					source_directory_name,source_file_name,target_directory_name,
-					config_directory_name,Charset.defaultCharset().name(),
-					precision,precision,max_step_number,external_flag
-				},null);
+					
+					source_file_name,target_directory_name,config_directory_name,
+					Charset.defaultCharset().name(),chordal_deflection_str,angular_deflection_str,
+					max_step_number_str
+				});
 		if((new File(target_directory_name+"token.txt")).exists()){
 			debug_information.println("Convert success,target directory: ",target_directory_name);
 			return false;
