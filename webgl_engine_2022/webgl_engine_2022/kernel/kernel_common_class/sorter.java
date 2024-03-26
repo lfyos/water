424e43e1c9d33237ab29f799e7fe054156e229b0
lfyos <lfy@shu.edu.cn>
2024-03-26 13:15:35 +0800

    commit-2024-03-26

--- webgl_engine_2022/webgl_engine_2022/kernel/kernel_common_class/sorter.java
+++ webgl_engine_2022/webgl_engine_2022/kernel/kernel_common_class/sorter.java
@@ -21,12 +21,10 @@ public class sorter <DATA_TYPE,KEY_TYPE>
 	}
 	public int search(KEY_TYPE t)
 	{
-		int search_id=-1;
 		if(data_array!=null)
 			for(int begin_pointer=0,end_pointer=data_array.length-1;begin_pointer<=end_pointer;){
-				search_id=(begin_pointer+end_pointer)/2;
-				int result=compare_key(data_array[search_id],t);
-				if(result<0)
+				int result,search_id=(begin_pointer+end_pointer)/2;
+				if((result=compare_key(data_array[search_id],t))<0)
 					begin_pointer=search_id+1;
 				else if(result>0)
 					end_pointer=search_id-1;
@@ -37,14 +35,13 @@ public class sorter <DATA_TYPE,KEY_TYPE>
 	}
 	public int[] range(KEY_TYPE t)
 	{
-		int begin_id,end_id,my_search_id;
-		if((my_search_id=search(t))<0)
+		int begin_id,end_id,end_number,search_id;
+		if((search_id=search(t))<0)
 			return null;
-		for(begin_id=my_search_id;begin_id>0;begin_id--)
+		for(begin_id=search_id;begin_id>0;begin_id--)
 			if(compare_key(data_array[begin_id-1],t)!=0)
 				break;
-		int end_number=data_array.length-1;
-		for(end_id=my_search_id;end_id<end_number;end_id++)
+		for(end_id=search_id,end_number=data_array.length-1;end_id<end_number;end_id++)
 			if(compare_key(data_array[end_id+1],t)!=0)
 				break;
 		return new int[]{begin_id,end_id};
@@ -57,35 +54,54 @@ public class sorter <DATA_TYPE,KEY_TYPE>
 	{
 		if(data_array==null)
 			return;
-		if(data_array.length<=1)
-			return;
-		
-		for(int i=1,part_number=data_array.length;i<part_number;i++)
-			for(int j=i;j>0;){
-				int parent=(j-1)/2;
-				if(compare_data(data_array[parent],data_array[j])>=0)
+		int part_number=data_array.length;
+		int id_array[]=new int[part_number];
+		for(int i=0;i<part_number;i++)
+			id_array[i]=i;
+
+		for(int i=1;i<part_number;i++)
+			for(int j=i,parent,result;j>0;j=parent){
+				if((result=compare_data(data_array[parent=(j-1)/2],data_array[j]))==0)
+					result=id_array[parent]-id_array[j];
+				if(result>=0)
 					break;
+				
+				result=id_array[parent];
+				id_array[parent]=id_array[j];
+				id_array[j]=result;
+				
 				DATA_TYPE p=data_array[parent];
 				data_array[parent]=data_array[j];	
 				data_array[j]=p;
-				j=parent;
 			}
-		for(int part_number=data_array.length,i=part_number-1;i>0;i--){
+		for(int i=part_number-1;i>0;i--){
+			int result=id_array[i];
+			id_array[i]=id_array[0];
+			id_array[0]=result;
+			
 			DATA_TYPE p=data_array[i];
 			data_array[i]=data_array[0];
 			data_array[0]=p;
-			for(int j=0,select_child,other_child;;){
-				if((select_child=j+j+1)>=i)
-					break;
-				if((other_child=select_child+1)<i)
-					if(compare_data(data_array[select_child],data_array[other_child])<=0)
+			
+			for(int j=0,select_child,other_child;(select_child=j+j+1)<i;j=select_child){
+				if((other_child=select_child+1)<i){
+					if((result=compare_data(data_array[select_child],data_array[other_child]))==0)
+						result=id_array[select_child]-id_array[other_child];
+					if(result<0)
 						select_child=other_child;
-				if((compare_data(data_array[j],data_array[select_child]))>=0)
+				}
+				if((result=compare_data(data_array[j],data_array[select_child]))==0)
+					result=id_array[j]-id_array[select_child];
+				if(result>=0)
 					break;
+				
+				result=id_array[j];
+				id_array[j]=id_array[select_child];
+				id_array[select_child]=result;
+				
 				p=data_array[j];
 				data_array[j]=data_array[select_child];
 				data_array[select_child]=p;
-				j=select_child;
 			}
 		}
 	}
