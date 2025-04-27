71a0e094b8349572b1c8ed6fb1e34ccd5339a19e
lfyos <lfy@shu.edu.cn>
2025-04-27 16:12:18 +0800

    commit-2025-04-27

--- webgpu_engine_2025/webgpu_engine/driver_general/driver_camera_operation/part.js
+++ webgpu_engine_2025/webgpu_engine/driver_general/driver_camera_operation/part.js
@@ -1,9 +1,8 @@
-function construct_part_driver(init_data,part_object,render_driver,render)
+function construct_part_driver(init_data,part_object,render_driver,scene)
 {
 	this.decode_vertex_data=function(request_type_string,buffer_object_data,part_object)
 	{
 		return buffer_object_data;
 	}
-
 	this.new_component_driver=construct_component_driver;
 }
