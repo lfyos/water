f8d739c6c887ad745a1783d143418791f83d0c2d
lfyos <lfy@shu.edu.cn>
2026-08-20 04:42:20 +0800

    commit-2026-08-19-3

--- webgpu_engine_2025/webgpu_engine/kernel/kernel_component/component_selection.java
+++ webgpu_engine_2025/webgpu_engine/kernel/kernel_component/component_selection.java
@@ -44,16 +44,18 @@ public class component_selection
 	private void clear_selected_flag_without_brother(component my_comp)
 	{
 		component_link_list component_link_list_head=null;
-		for(;my_comp!=null;my_comp=component_cont.get_component(my_comp.parent_component_id)){
-			component_link_list_head=new component_link_list(my_comp,0,component_link_list_head);
-			if(my_comp.uniparameter.selected_flag){
-				my_comp.uniparameter.selected_flag=false;
+		for(component my_parent=my_comp;my_parent!=null;
+				my_parent=component_cont.get_component(my_parent.parent_component_id))
+		{
+			component_link_list_head=new component_link_list(my_parent,0,component_link_list_head);
+			if(my_parent.uniparameter.selected_flag){
+				my_parent.uniparameter.selected_flag=false;
 				for(;component_link_list_head.next_list_item!=null;
 						component_link_list_head=component_link_list_head.next_list_item)
 					for(int i=0,ni=component_link_list_head.comp.children.size();i<ni;i++) {
-						my_comp=component_link_list_head.comp.children.get(i);
-						if(component_link_list_head.next_list_item.comp!=my_comp)
-							my_comp.uniparameter.selected_flag=true;
+						component my_child=component_link_list_head.comp.children.get(i);
+						if(component_link_list_head.next_list_item.comp!=my_child)
+							my_child.uniparameter.selected_flag=true;
 					}
 				return;
 			}
@@ -67,9 +69,9 @@ public class component_selection
 	public void clear_selected_flag(component my_comp)
 	{
 		if(my_comp!=null){
-			for(component comp=my_comp;comp!=null;
-					comp=component_cont.get_component(comp.parent_component_id))
-				if(comp.uniparameter.selected_flag){
+			for(component my_parent=my_comp;my_parent!=null;
+					my_parent=component_cont.get_component(my_parent.parent_component_id))
+				if(my_parent.uniparameter.selected_flag){
 					clear_selected_flag_without_brother(my_comp);
 					return;
 				}
@@ -78,9 +80,9 @@ public class component_selection
 	}
 	public void switch_selected_flag(component my_comp)
 	{
-		for(component comp=my_comp;comp!=null;
-				comp=component_cont.get_component(comp.parent_component_id))
-			if(comp.uniparameter.selected_flag){
+		for(component my_parent=my_comp;my_parent!=null;
+				my_parent=component_cont.get_component(my_parent.parent_component_id))
+			if(my_parent.uniparameter.selected_flag){
 				clear_selected_flag_without_brother(my_comp);
 				return;
 			}
@@ -96,10 +98,10 @@ public class component_selection
 	}
 	public void set_child_selected(component my_comp)
 	{
-		for(component comp=my_comp;comp!=null;
-			comp=component_cont.get_component(comp.parent_component_id))
+		for(component my_parent=my_comp;my_parent!=null;
+				my_parent=component_cont.get_component(my_parent.parent_component_id))
 		{
-			if(comp.uniparameter.selected_flag) {
+			if(my_parent.uniparameter.selected_flag) {
 				clear_selected_flag_without_brother(my_comp);
 				break;
 			}
